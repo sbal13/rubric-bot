490645f04ab85a42121f35115ab5d992f22aff26
sbal13 <sbal13@gmail.com>
2018-11-07 11:23:14 -0500

    update ruby version

--- app/models/google_calendar.rb
+++ app/models/google_calendar.rb
@@ -18,64 +18,64 @@ class GoogleCalendar
 # the user's default browser will be launched to approve the request.
 #
 # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-	def self.authorize
-		client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
-		token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-		authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-		user_id = 'default'
-		credentials = authorizer.get_credentials(user_id)
-		if credentials.nil?
-			url = authorizer.get_authorization_url(base_url: OOB_URI)
-			puts 'Open the following URL in the browser and enter the ' \
-					 "resulting code after authorization:\n" + url
-			code = gets
-			credentials = authorizer.get_and_store_credentials_from_code(
-				user_id: user_id, code: code, base_url: OOB_URI
-			)
-		end
-		credentials
-	end
+  def self.authorize
+    client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
+    authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
+    user_id = 'default'
+    credentials = authorizer.get_credentials(user_id)
+    if credentials.nil?
+      url = authorizer.get_authorization_url(base_url: OOB_URI)
+      puts 'Open the following URL in the browser and enter the ' \
+           "resulting code after authorization:\n" + url
+      code = gets
+      credentials = authorizer.get_and_store_credentials_from_code(
+        user_id: user_id, code: code, base_url: OOB_URI
+      )
+    end
+    credentials
+  end
 
-	def self.get_calendars
-		service = Google::Apis::CalendarV3::CalendarService.new
-		service.client_options.application_name = APPLICATION_NAME
-		service.authorization = self.authorize
+  def self.get_calendars
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.client_options.application_name = APPLICATION_NAME
+    service.authorization = self.authorize
 
-		calendars = service.list_calendar_lists
+    calendars = service.list_calendar_lists
 
-		selected_calendars = []
-		calendars.items.each do |calendar|
-			if (calendar.id.include?("group.calendar.google.com") && calendar.summary.downcase.include?("web-"))
-				selected = {id: calendar.id, name: calendar.summary.downcase}
-				selected_calendars << selected
-			end
-		end
+    selected_calendars = []
+    calendars.items.each do |calendar|
+      if (calendar.id.include?("group.calendar.google.com") && calendar.summary.downcase.include?("web-"))
+        selected = {id: calendar.id, name: calendar.summary.downcase}
+        selected_calendars << selected
+      end
+    end
 
-		calendars_and_events = {}
-		selected_calendars.each do |calendar_data|
-			calendars_and_events[calendar_data[:name]] = self.get_events(calendar_data, service)
-		end
+    calendars_and_events = {}
+    selected_calendars.each do |calendar_data|
+      calendars_and_events[calendar_data[:name]] = self.get_events(calendar_data, service)
+    end
 
-		calendars_and_events
-	end
+    calendars_and_events
+  end
 
 
 
-	def self.get_events(calendar_data, service)
-		response = service.list_events(calendar_data[:id],
-																	 max_results: 30,
-																	 single_events: true,
-																	 order_by: 'startTime',
-																	 time_min: Time.now.iso8601,
-																	 time_max: (Time.now + (60 * 60 * 24)).iso8601)
-		puts 'Upcoming events:'
-		puts 'No upcoming events found' if response.items.empty?
-		
-		lectures = response.items.select do |event|
-			event.summary.downcase.include?("lecture")
-		end
+  def self.get_events(calendar_data, service)
+    response = service.list_events(calendar_data[:id],
+                                   max_results: 30,
+                                   single_events: true,
+                                   order_by: 'startTime',
+                                   time_min: Time.now.iso8601,
+                                   time_max: (Time.now + (60 * 60 * 24)).iso8601)
+    puts 'Upcoming events:'
+    puts 'No upcoming events found' if response.items.empty?
+    
+    lectures = response.items.select do |event|
+      event.summary.downcase.include?("lecture")
+    end
 
-		return lectures
-	end
+    return lectures
+  end
 end
 
--- db/migrate/20181019014700_create_lecture_reviewers.rb
+++ db/migrate/20181019014700_create_lecture_reviewers.rb
@@ -1,12 +1,12 @@
 class CreateLectureReviewers < ActiveRecord::Migration[5.2]
-	def change
-		create_table :lecture_reviewers do |t|
-			t.integer :reviewer_id
-			t.integer :lecture_id
-			t.boolean :reviewed
-			t.boolean :sent_invitation
+  def change
+    create_table :lecture_reviewers do |t|
+      t.integer :reviewer_id
+      t.integer :lecture_id
+      t.boolean :reviewed
+      t.boolean :sent_invitation
 
-			t.timestamps
-		end
-	end
+      t.timestamps
+    end
+  end
 end
