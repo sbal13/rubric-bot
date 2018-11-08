5c84420a7d1b1e49d9081358a38b2d87c1f7fe27
sbal13 <sbal13@gmail.com>
2018-11-08 08:58:46 -0500

    rake task

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,28 +1,28 @@
 
 class CalendarController < ApplicationController
-  def get_cal
-    calendar_lectures = CalendarInviter.get_data
-    
-    calendar_lectures.each do |cohort_name, lectures|
-      lectures.each do |lecture|
-        lecture.lecture_reviewers.each do |lr|
-          if !lr.sent_invitation 
-            MailjetMailer.send(lr.reviewer, lecture)
-            lr.update(sent_invitation: true)
-          end
-        end
-      end
-    end
+	def get_cal
+		calendar_lectures = CalendarInviter.get_data
+		
+		calendar_lectures.each do |cohort_name, lectures|
+			lectures.each do |lecture|
+				lecture.lecture_reviewers.each do |lr|
+					if !lr.sent_invitation 
+						MailjetMailer.send(lr.reviewer, lecture)
+						lr.update(sent_invitation: true)
+					end
+				end
+			end
+		end
 
-    render json: calendar_lectures
-  end
+		render json: calendar_lectures
+	end
 
-  # def scripts
+	# def scripts
 
-  #   response = GoogleScripts.fetch
+	#   response = GoogleScripts.fetch
 
-  #   render json: response
-  # end
+	#   render json: response
+	# end
 end
 
 
